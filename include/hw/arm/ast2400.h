3af7fd543a5e55d6b9180052be26c7705349fbad
Andrew Jeffery <andrew@aj.id.au>
2016-05-11 12:55:07 +0930

    ast2400: Add on-chip SRAM
    This was extracted from Teddy Reed's commit adding the fbyosemite
    machine.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- include/hw/arm/ast2400.h
+++ include/hw/arm/ast2400.h
@@ -23,6 +23,7 @@ typedef struct AST2400State {
 
     /*< public >*/
     ARMCPU *cpu;
+    MemoryRegion sram;
     MemoryRegion iomem;
     AspeedVICState vic;
     AspeedTimerCtrlState timerctrl;
@@ -33,5 +34,7 @@ typedef struct AST2400State {
 #define AST2400(obj) OBJECT_CHECK(AST2400State, (obj), TYPE_AST2400)
 
 #define AST2400_SDRAM_BASE       0x40000000
+#define AST2400_SRAM_BASE        0x1E720000
+#define AST2400_SRAM_SIZE        0x00008000 /* 32kB */
 
 #endif /* AST2400_H */
